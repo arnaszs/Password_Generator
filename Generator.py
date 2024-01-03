a4785af0044f4044a0b2d99a4a43cb57f35967f2
arnaszs <bolisas8@gmail.com>
2024-01-03 14:16:50 +0200

    code polish

--- Generator.py
+++ Generator.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import random
 import string
 
+
 def generate_password():
     length = int(length_entry.get())
 
@@ -14,6 +15,7 @@ def generate_password():
     password = ''.join(random.choice(characters) for _ in range(length))
     password_entry.delete(0, tk.END)
     password_entry.insert(0, password)
+
 
 def copy_password():
     password = password_entry.get()
@@ -30,6 +32,7 @@ window = tk.Tk()
 window.title("Password Generator")
 window.geometry("275x250")  # Set initial window size
 
+
 # Style configuration
 background_colour = "#3498db"  # Blue colour
 fg_colour = "#ffffff"  # White colour
@@ -37,8 +40,10 @@ button_colour = "#2ecc71"  # Green colour for buttons
 button_text_colour = "#ffffff"  # White colour for button text
 colour_entry_fields = "#ecf0f1"  # Light gray background for entry fields
 
+
 # Set background color for the entire window
 window.configure(bg=background_colour)
+
 
 # Length Label and Entry
 length_label = tk.Label(window, text="Password Length:", bg=background_colour, fg=fg_colour)
@@ -47,19 +52,23 @@ length_label.grid(row=0, column=0, padx=10, pady=10)
 length_entry = tk.Entry(window, bg=colour_entry_fields)
 length_entry.grid(row=0, column=1, padx=10, pady=10)
 
+
 # Generate Button
 generate_button = tk.Button(window, text="Generate Password", command=generate_password,
                             bg=button_colour, fg=button_text_colour, relief=tk.GROOVE)
 generate_button.grid(row=1, column=0, columnspan=2, pady=10)
 
+
 # Password Entry
 password_entry = tk.Entry(window, bg=colour_entry_fields)
 password_entry.grid(row=2, column=0, columnspan=2, pady=10)
+
 
 # Copy Password Button
 copy_button = tk.Button(window, text="Copy Password", command=copy_password,
                         bg=button_colour, fg=button_text_colour, relief=tk.GROOVE)
 copy_button.grid(row=3, column=0, columnspan=2, pady=10)
+
 
 # Run the Tkinter event loop
 window.mainloop()
